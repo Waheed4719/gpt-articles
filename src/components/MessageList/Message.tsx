9e76ef97e76d15ec055119fd96b93e777930451d
Waheed Uddin Ahmed <dmc4719@gmail.com>
2023-04-13 08:33:41 +0600

    Fix text formatting

--- src/components/MessageList/Message.tsx
+++ src/components/MessageList/Message.tsx
@@ -7,13 +7,48 @@ import MessageAuthor from './MessageAuthor'
 type Props = {
   message: MessageObjectType
 }
+const pattern = /```([^`]*)```/g
+
+const formatCode = (item: string, index: number) => {
+  const delimiter = '\n'
+
+  const idx = item.indexOf(delimiter)
+  const language = item.substring(0, idx)
+  const formattedLanguage =
+    language.substring(0, idx).charAt(0).toUpperCase() +
+    language.substring(0, idx).slice(1)
+  const code = item.substring(idx + 1)
+  return (
+    <>
+      <div className='text-white flex items-center justify-between rounded-t-md h-[40px] bg-[#343541] px-[20px] py-2'>
+        <p className='text-[12px] font-semibold'>
+          {formattedLanguage === '' ? 'Unknown language' : formattedLanguage}
+        </p>
+        <button
+          type='button'
+          className='hover:text-gray-200 text-[12px] font-semibold'
+        >
+          Copy code
+        </button>
+      </div>
+      <SyntaxHighlighter
+        key={index}
+        language={language ?? 'javascript'}
+        style={dracula}
+        className=' p-[20px_!important] rounded-b-md'
+      >
+        {code}
+      </SyntaxHighlighter>
+    </>
+  )
+}
 
 const Message = ({ message }: Props) => {
   return (
     <div key={message.message} className='flex flex-col bg-transparent'>
       {message.sender === 'ChatGPT' ? (
         <div className='flex flex-col p-[20px]'>
-          <div className='relative flex gap-3 rounded-xl w-full md:w-[800px] mx-auto mt-1'>
+          <div className='relative flex gap-4 rounded-xl w-full md:w-[800px] mx-auto mt-1'>
             <MessageAuthor sender='ChatGPT' />
             <div className='text-sm text-white leading-[28px] flex flex-col flex-1 w-[calc(100%-80%)]'>
               {message.message.split('```').map((item, index) => {
@@ -28,37 +63,7 @@ const Message = ({ message }: Props) => {
                     />
                   )
                 } else {
-                  const delimiter = '\n'
-
-                  const idx = item.indexOf(delimiter)
-                  const language = item.substring(0, idx)
-                  const formattedLanguage =
-                    item.substring(0, idx).charAt(0).toUpperCase() +
-                    item.substring(0, idx).slice(1)
-                  const code = item.substring(idx + 1)
-                  return (
-                    <>
-                      {formattedLanguage && (
-                        <div className='text-white flex items-center justify-between rounded-t-md h-[40px] bg-[#343541] px-[20px] py-2'>
-                          <p className='text-sm'>{formattedLanguage}</p>
-                          <button
-                            type='button'
-                            className='hover:text-gray-200 text-sm'
-                          >
-                            Copy code
-                          </button>
-                        </div>
-                      )}
-                      <SyntaxHighlighter
-                        key={index}
-                        language={language ?? 'javascript'}
-                        style={dracula}
-                        className=' p-[20px_!important] rounded-b-md'
-                      >
-                        {code}
-                      </SyntaxHighlighter>
-                    </>
-                  )
+                  return formatCode(item, index)
                 }
               })}
             </div>
@@ -68,9 +73,12 @@ const Message = ({ message }: Props) => {
         <div className='flex flex-col  p-[20px] bg-[#343541]'>
           <div className='relative flex gap-3 rounded-xl w-full md:w-[800px]  mx-auto mt-1'>
             <MessageAuthor sender='User' />
-            <p className='text-sm text-white leading-[28px]'>
-              {message.message}
-            </p>
+            <p
+              className='mb-4 text-sm text-white leading-[28px]'
+              dangerouslySetInnerHTML={{
+                __html: message.message.replace(/\n\n/g, '<br/>'),
+              }}
+            />
           </div>
         </div>
       )}
